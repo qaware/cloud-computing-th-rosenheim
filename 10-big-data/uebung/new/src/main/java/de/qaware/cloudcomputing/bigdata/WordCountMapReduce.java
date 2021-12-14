50e7d824f82aaa896e128ccec2005734591b2189
Franz Wimmer <franz.wimmer@qaware.de>
2021-12-14 22:51:30 +0100

    Prefer IPv4
    Former-commit-id: 03bda44ea27384859177bd644f0a5d3442eeb380
    Former-commit-id: a44b9cca6635a3310b7a4a3f7bcbff30fad1dfbc

--- 10-big-data/uebung/new/src/main/java/de/qaware/cloudcomputing/bigdata/WordCountMapReduce.java
+++ 10-big-data/uebung/new/src/main/java/de/qaware/cloudcomputing/bigdata/WordCountMapReduce.java
@@ -2,21 +2,24 @@ package de.qaware.cloudcomputing.bigdata;
 
 import de.qaware.cloudcomputing.bigdata.adapter.WordCountSplitAdapter;
 import de.qaware.cloudcomputing.bigdata.ignite.IgniteConfigurationProvider;
-import org.apache.commons.io.IOUtils;
+import de.qaware.cloudcomputing.bigdata.util.FileUtil;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteException;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.configuration.IgniteConfiguration;
 
-import java.io.IOException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class WordCount {
+public class WordCountMapReduce {
+
+    static {
+        System.getProperties().setProperty("java.net.preferIPv4Stack", "true");
+    }
+
+    private static final int WORD_LENGTH_MIN = 5;
 
     private static final String FILENAME = "ulysses.txt";
 
@@ -26,7 +29,7 @@ public class WordCount {
         // Starting the node
         Ignite ignite = Ignition.start(igniteConfiguration);
 
-        String book = readFile();
+        String book = FileUtil.readFileFromResources(FILENAME);
 
         WordCountSplitAdapter wordCountSplitAdapter = new WordCountSplitAdapter();
 
@@ -34,27 +37,18 @@ public class WordCount {
         Map<String, Integer> sortedTaskResult = sortMapByValue(taskResult);
 
         for (Map.Entry<String, Integer> entry : sortedTaskResult.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+            System.out.format("| %-20s | %-5s |%n", entry.getKey(), entry.getValue());
         }
 
         // Disconnect from the cluster.
         ignite.close();
     }
 
-    private static String readFile() {
-        try {
-            URL url = WordCount.class.getClassLoader().getResource(FILENAME);
-            return IOUtils.toString(url, StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     private static Map<String, Integer> sortMapByValue(Map<String, Integer> map) {
         return map.entrySet().stream()
-                .filter(word -> word.getKey().length() > 10)
+                .filter(word -> word.getKey().length() > WORD_LENGTH_MIN)
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                .limit(20)
+                .limit(30)
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
     }
 }
